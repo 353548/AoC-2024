eee6f29330358936c44d54f884219b157d465b6c
353548 <ad6127@disroot.org>
2024-12-03 15:54:42 +0100

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,20 @@
+with open('input.txt', 'r') as f:
+    pairs = [list(map(int, numbers.strip().split())) for numbers in f.readlines()]
+
+left_side = []
+right_side = []
+
+for pair in pairs:
+    left_side.append(pair[0])
+    right_side.append(pair[1])
+
+left_side = sorted(left_side)
+right_side = sorted(right_side)
+
+difference = []
+
+for index in range(len(pairs)):
+    apart = abs(left_side[index] - right_side[index])
+    difference.append(apart)
+
+print(sum(difference))
